6461f3e39eea205d08fb4e73cd217052df4c0124
aronesura <aronesguerra0508@gmail.com>
2024-03-11 11:53:54 +0800

    [B] Hotfix for storybook issue on custom hooks

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,5 +1,6 @@
 // Replace your-framework with the webpack-based framework you are using (e.g., react-webpack5)
 import type { StorybookConfig } from '@storybook/react-webpack5';
+import * as path from 'path';
 
 const config: StorybookConfig = {
   framework: {
@@ -20,6 +21,21 @@ const config: StorybookConfig = {
       },
     },
   }),
+
+  webpackFinal: async (config) => {
+    // add SCSS support for CSS Modules
+    config.resolve = {
+      ...(config.resolve || {}),
+      extensions: ['.ts', '.tsx', '.js'],
+      alias: {
+        components: path.resolve(__dirname, '../src', 'components'),
+        hooks: path.resolve(__dirname, '../src', 'hooks'),
+        types: path.resolve(__dirname, '../src', 'types'),
+      },
+    };
+
+    return config;
+  },
 
   stories: ['../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
 };
